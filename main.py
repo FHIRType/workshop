c6522fef39fd3ac64029f2d53343db71be673da2
Hla Htun <kshap.karki@gmail.com>
2023-11-14 12:00:34 -0800

    Any type of practitioner type can be passed (form either kaiser or humana) and it will return a list object with all the important information that is available in each specific query/resource

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-# Authors: Iain Richey, Trenton Young, Kevin Carman
+# Authors: Iain Richey, Trenton Young, Kevin Carman, Hla Htun
 # Description: Much of the functionality borrowed from code provided by Kevin.
 
 import json
 
 from endpoint import Endpoint
 from client import SmartClient
+from norm import getKaiserData, getHumanaData
 
 endpoint_humana = Endpoint("Humana", "fhir.humana.com", "/sandbox/api/")  # Or "/api/"
 endpoint_kaiser = Endpoint("Kaiser", "kpx-service-bus.kp.org", "/service/hp/mhpo/healthplanproviderv1rc/")
@@ -12,16 +13,25 @@ endpoint_cigna = Endpoint("Cigna", "p-hi2.digitaledge.cigna.com", "/ProviderDire
 endpoint_centene = Endpoint("Centene", "production.api.centene.com", "/fhir/providerdirectory/", False)
 endpoint_pacificsource = Endpoint("Pacific Source", "api.apim.pacificsource.com", "/fhir/provider/R4/")
 
+endpoints = [
+    endpoint_humana,
+    endpoint_centene,
+    endpoint_cigna,
+    endpoint_kaiser,
+    endpoint_pacificsource
+]
+
 # +19087212277 <- ideal
 def somefunc(oldphonenumber):
     return "fixed phone number"
-endpoint_pacificsource.set_mask(endpoint_pacificsource.get_location_str()+"_phonenumber", lambda oldphonenumber : somefunc(oldphonenumber))
-badphone = "+(1212)334313"
-print(endpoint_pacificsource.use_mask(endpoint_pacificsource.get_location_str()+"_phonenumber", badphone))
+
+
+def stan_name(nx):
+    return nx
 
 provider_lookup_name_data = [
-    # {"f_name": "Brandon", "l_name": "Bianchini", "NPI": "1700158326", "prac_resp": "None", "prac_role_resp": "None",
-    #  "loc_resp": "None"},
+    {"f_name": "Brandon", "l_name": "Bianchini", "NPI": "1700158326", "prac_resp": "None", "prac_role_resp": "None",
+     "loc_resp": "None"},
     {"f_name": "Kaydie", "l_name": "Satein", "NPI": "1619302171", "prac_resp": "None", "prac_role_resp": "None",
      "loc_resp": "None"},
     {"f_name": "Toren", "l_name": "Davis", "NPI": "1457779498", "prac_resp": "None", "prac_role_resp": "None",
@@ -38,12 +48,12 @@ provider_lookup_name_data = [
      "loc_resp": "None"},
     {"f_name": "Natasha", "l_name": "Ingvoldstad-O'Neal", "NPI": "1689871147", "prac_resp": "None",
      "prac_role_resp": "None", "loc_resp": "None"},
-    # {"f_name": "Michael", "l_name": "Liu", "NPI": "1841210549", "prac_resp": "None", "prac_role_resp": "None",
-    #  "loc_resp": "None"},
-    # {"f_name": "David", "l_name": "Paik", "NPI": "1023218047", "prac_resp": "None", "prac_role_resp": "None",
-    #  "loc_resp": "None"},
-    # {"f_name": "Adriana", "l_name": "Linares", "NPI": "1558577130", "prac_resp": "None", "prac_role_resp": "None",
-    #  "loc_resp": "None"}
+    {"f_name": "Michael", "l_name": "Liu", "NPI": "1841210549", "prac_resp": "None", "prac_role_resp": "None",
+     "loc_resp": "None"},
+    {"f_name": "David", "l_name": "Paik", "NPI": "1023218047", "prac_resp": "None", "prac_role_resp": "None",
+     "loc_resp": "None"},
+    {"f_name": "Adriana", "l_name": "Linares", "NPI": "1558577130", "prac_resp": "None", "prac_role_resp": "None",
+     "loc_resp": "None"}
 ]
 
 
@@ -56,6 +66,12 @@ def print_resource(resource):
     print(json.dumps(resource.as_json(), sort_keys=False, indent=2))
 
 
+def print_res_obj(obj):
+    for res in obj:
+        print(res, ": ", obj[res])
+    print("\n")
+
+
 def main():
     smartclient_humana = SmartClient(endpoint_humana)
     smartclient_centene = SmartClient(endpoint_centene)
@@ -63,30 +79,33 @@ def main():
     smartclient_kaiser = SmartClient(endpoint_kaiser)
     smartclient_pacificsource = SmartClient(endpoint_pacificsource)
 
-    clients = [
+    smart_clients = [
         smartclient_humana,
         smartclient_centene,
         smartclient_cigna,
         smartclient_kaiser,
         smartclient_pacificsource
-                 ]
+    ]
 
     # for _client in clients:
     #     req = "metadata"
     #     query = "GET " + _client.get_endpoint_url() + req
     #     print(query, _client.http_query(req), sep=" | ")
 
-    for client in clients:
+    for client in smart_clients:
         print("\n  ####  ", client.get_endpoint_name(), "  ####")
         for data in provider_lookup_name_data:
-            # print_resource(smartclient_humana.find_provider(data["f_name"], data["l_name"], data["NPI"]))
-            i = client.find_provider(data["f_name"], data["l_name"], data["NPI"])
-            if i:
-                print("\n", i, end="")
+            resource = client.find_provider(data["f_name"], data["l_name"], data["NPI"])
+            if resource:
+                print('\n')
+                # print_resource(resource)
+                if client == smartclient_humana:
+                    print_res_obj(getHumanaData(resource))
+                elif client == smartclient_kaiser:
+                    print_res_obj(getKaiserData(resource))
             else:
                 print("...", end="")
 
 
 if __name__ == "__main__":
     main()
-

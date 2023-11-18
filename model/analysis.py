821617b120feae06ef95f73bcf6718ea5971354b
Richeyi <91440464+Richeyi@users.noreply.github.com>
2023-11-18 15:09:51 -0800

    Create analysis.py
    Started working on an analysis

---
+++ model/analysis.py
@@ -0,0 +1,8 @@
+
+####################
+#This function takes in a set of queries to the various endpoints, and analyses them for the
+# most likely results 
+#
+####################
+def predict(query):
+    print("in predict")

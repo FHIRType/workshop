4ff1e07bb3a9dde985e3cf3d65254ca07c86ec02
valdovjo <valdovjo@oregonstate.edu>
2023-11-19 17:45:32 -0800

    Merging onto DEV_Logging

--- logging_fhir.py
+++ logging_fhir.py
@@ -1,75 +1,24 @@
-#Author: Dani Valdovinos
-
 import logging
-import datetime as dt
-import json
-from endpoint import Endpoint
-from client import SmartClient
-# import time
+import logging.config
+import sys
+from logging.handlers import TimedRotatingFileHandler
+from datetime import datetime
 
-def initialize_logger(filename):
-    """
-    Initialize the logger object and configure file handling.
-    """
+# Load the logging configuration
+logging.config.fileConfig("../logging/logging.ini")
 
-    logging.basicConfig(level=logging.DEBUG)
+# Get the logger
+logger = logging.getLogger('fhir')
 
-    logger = logging.getLogger("FHIR")
-    
-    #do we want to log and append log messages each time it runs the code
-    #or do we want to get rid off the old messages and have a fresh log for each day
-    # Use 'w' mode to write to a new file each time (overwrite existing)
-    # file_handler = logging.FileHandler(filename, mode='w')
+# Dynamically set up TimedRotatingFileHandler with today's date
+log_file_name = f"{datetime.now().strftime('%Y-%m-%d')}_fhir.log"
+file_handler = TimedRotatingFileHandler(log_file_name, when='midnight', interval=1, backupCount=30, utc=True)
+file_handler.setFormatter(logging.Formatter("%(asctime)s: %(filename)s %(levelname)s - %(lineno)d %(message)s", datefmt="%Y-%m-%d %I:%M:%S %p"))
+logger.addHandler(file_handler)
 
-    file_handler = logging.FileHandler(filename)
-
-    # file_handler.setLevel(logging.DEBUG)
-
-    formatter = logging.Formatter("%(asctime)s: %(filename)s %(levelname)s - %(lineno)d %(message)s",  datefmt="%Y-%m-%d %I:%M:%S %p")
-    file_handler.setFormatter(formatter)
-
-    logger.addHandler(file_handler)
-    return logger
-
-
-
-    #we could create different filenames to display other information
-    #perf_handler = logging.FileHandler(f"performance_{filename}", mode='w')  # Separate file for performance logs
-    #perf_handler.setLevel(logging.DEBUG)
-
-    #perf_formatter = logging.Formatter("%(asctime)s: %(levelname)s - %(message)s - Duration: %(duration)s seconds", datefmt="%Y-%m-%d %I:%M:%S %p")
-    #perf_handler.setFormatter(perf_formatter)
-
-    #logger.addHandler(perf_handler)
-    
-
-def record_log_entries(logger):
-    """
-    Record sample log entries using the provided logger.
-    """
-    logger.debug("This is debug")
-    logger.info("This is info")
-    logger.warning("This is a warning")
-    logger.error("There is an error")
-    logger.critical("This is critical")
-
-
-#we can log events within our code such as:
-# - logging any errors with connections from endpoints using exceptions or stack traces
-# - logging for tracking changes: updates, removing of records
-# - logging for performance such as response times for each request, query durations from database
-
-if __name__ == "__main__":
-    today = dt.datetime.today()
-    filename = f"{today.month:02d}-{today.day:02}-{today.year}.log" #ex: 11-14-2023
-
-    # logging.basicConfig(level=logging.DEBUG)
-
-    #initialize logger
-    logger = initialize_logger(filename)
-
-    #record log entries
-    record_log_entries(logger)
-
-    #endpoint interaction
-    # fhir_endpoint_interaction()
+# Log some messages
+logger.debug("debug message")
+logger.info("info message")
+logger.warning("warning message")
+logger.error("error message")
+logger.critical("critical message")

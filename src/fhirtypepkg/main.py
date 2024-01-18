a1fd0552a6d02b6378ee445c1e6961b5339a818b
Hla Htun <kshap.karki@gmail.com>
2024-01-18 11:31:23 -0800

    API now prints out flattened response back along with the queried responses and the predicted response

--- src/fhirtypepkg/main.py
+++ src/fhirtypepkg/main.py
@@ -92,12 +92,15 @@ try:
 except psycopg2.OperationalError:
     pass
 
-def print_all(all_results, predicted):
+def print_all(all_results, predicted, flat_data=None):
     if all_results and predicted:
         print("\n\nAll results")
         print_resource(all_results)
         print("\n\nPredicted Result")
         print_resource(predicted)
+        if flat_data is not None:
+            print("\n\nFlattened :(")
+            print_res_obj(flat_data)
     else:
         print("\nNo matching results :(")
         print("\nHence, no predicted result as well:(\n\n")
@@ -162,6 +165,7 @@ def search_practitioner(
     """
     responses = []
     consensus_data = []
+    predicted_response = None
 
     for client_name, client in smart_clients.items():
         print("CLIENT NAME IS ", client_name)
@@ -175,17 +179,19 @@ def search_practitioner(
         responses.extend(practitioners)
         consensus_data.extend(filtered_data)
 
-    predicted_prac_id, predicted_prac = (
+    predicted_prac_id, predicted_flat_response = (
         predict(consensus_data) if consensus_data else (None, None)
     )
 
     if predicted_prac_id is not None:
+        # This changes the flat format to FHIR object
         for res in responses:
             if res.id == predicted_prac_id:
-                predicted_prac = res
+                predicted_response = res
                 break
 
-    return responses, [predicted_prac] if responses else None
+    # return responses, [predicted_prac] if responses else None
+    return responses, [predicted_response], predicted_flat_response if responses else None
 
 
 def search_practitioner_role(
@@ -364,10 +370,10 @@ async def main():
 
             if resource == "practitioner":
                 if dict_has_all_keys(params, ["family_name", "given_name", "npi"]):
-                    all_results, predicted = search_practitioner(
+                    all_results, predicted, flat_response = search_practitioner(
                         params["family_name"], params["given_name"], params["npi"]
                     )
-                    print_all(all_results, predicted)
+                    print_all(all_results, predicted, flat_response)
 
                 elif dict_has_all_keys(params, ["family_name", "given_name"]):
                     all_results, predicted = search_practitioner(

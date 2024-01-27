2a0978eef48e4444ea8d9f6f6a3dc43d884c2f8e
valdovjo <valdovjo@oregonstate.edu>
2024-01-27 11:11:01 -0800

    Adding example JSON data into file

--- src/flaskUI/app.py
+++ src/flaskUI/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, send_file, jsonify
 import json
+import os
 
 # import fhirtypepkg
 # from fhirtypepkg.client import SmartClient
@@ -59,7 +60,7 @@ print(deserialized_data)
 ##Feed in json object through post
 ##or be able to accept an object from model and display it
 
-##modul view controller
+##model view controller
 
 
 @app.route("/")
@@ -77,11 +78,51 @@ def index():
 
 
 """
-TODO:
 Download list of object to a file by:
 - Calling a function that creates the file
 - Serve the file for download
 """
+
+
+
+@app.route("/download", methods=["GET"])
+def download_file():
+
+    # Create a temp JSON file
+    # Give it a specific path
+    temp_file_path = f"data_files/temp_data.json"
+    with open(temp_file_path, "w") as f:
+        json.dump(data, f, indent=2)
+
+    # Serve the file for download
+    return send_file(temp_file_path, as_attachment=True)
+
+
+# Unit test for checking if data matches in the downloaded file
+
+def test_download_same_data():
+    # Simulate a request to the download endpoint
+    response = app.test_client().get('/download')
+
+    # Check if the response status code is 200
+    assert response.status_code == 200
+
+    # Check if the Content-Disposition header is set to attachment
+    assert 'attachment' in response.headers['Content-Disposition']
+
+    # Save the downloaded file content
+    with open("downloaded_data.json", "wb") as f:
+        f.write(response.data)
+
+    # Read the content of the downloaded file
+    with open("downloaded_data.json", "r") as f:
+        downloaded_data = json.load(f)
+
+    # Check if the downloaded data matches the original data
+    assert downloaded_data == data
+
+    # Clean up the temporary file
+    os.remove("downloaded_data.json")
 
 
 if __name__ == "__main__":

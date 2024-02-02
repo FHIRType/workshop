f02882a62d376433a82f974c418da731c5d8bbdf
valdovjo <valdovjo@oregonstate.edu>
2024-02-02 11:41:29 -0800

    Creating GET and POST methods for displaying data and downloading data

--- src/flaskUI/app.py
+++ src/flaskUI/app.py
@@ -1,22 +1,18 @@
-from flask import Flask, render_template, send_file, jsonify
+from flask import Flask, render_template, send_file, jsonify, request
 import json
 import os
-
-# import fhirtypepkg
-# from fhirtypepkg.client import SmartClient
+from io import BytesIO
 
 app = Flask(__name__)
 
-# Create an instance of SmartClient class
-# smart_client = SmartClient()
-
 
 """
-Example data
+Example data that is a list of dictionaries with different attributes
 """
 
-data = {
-    "Endpoint": "kpx-service-bus.kp.org/service/hp/mhpo/healthplanproviderv1rc/",
+# Test data
+test_data = [{
+    "Endpoint": "testEndpoint",
     "DateRetrieved": "01-21-2024",
     "FullName": "John Smith",
     "NPI": "0112031311",
@@ -39,90 +35,153 @@ data = {
     "LastPracRoleUpdate": "LastPracRoleUpdate_data",
     "LastLocationUpdate": "LastLocationUpdate_data",
     "AccuracyScore": "acc_score_data"
-}
+},
+{
+    "Endpoint": "testEndpoint2",
+    "DateRetrieved": "02-01-2024",
+    "FullName": "Jeff Johnson",
+    "NPI": "0131221",
+    "FirstName": "Jeff",
+    "LastName": "Johnson",
+    "Gender": "Male",
+    "Taxonomy": "X012311",
+    "GroupName": "Physician",
+    "ADD1": "1311 SW Ave",
+    "ADD2": "4311 NW Ave",
+    "City": "Austin",
+    "State": "Texas",
+    "Zip": "12234",
+    "Phone": "5031311311",
+    "Fax": "5031233234",
+    "Email": "jeffbezos@gmail.com",
+    "lat": "lat_data",
+    "lng": "lng_data",
+    "LastPracUpdate": "LastPracUpdate_data",
+    "LastPracRoleUpdate": "LastPracRoleUpdate_data",
+    "LastLocationUpdate": "LastLocationUpdate_data",
+    "AccuracyScore": "acc_score_data"
+},
+{
+    "Endpoint": "testEndpoint3",
+    "DateRetrieved": "02-02-2024",
+    "FullName": "Alice Johnson",
+    "NPI": "0213456",
+    "FirstName": "Alice",
+    "LastName": "Johnson",
+    "Gender": "Female",
+    "Taxonomy": "X045678",
+    "GroupName": "Pediatrician",
+    "ADD1": "789 Elm St",
+    "ADD2": "Apt 202",
+    "City": "Los Angeles",
+    "State": "California",
+    "Zip": "90001",
+    "Phone": "2135557890",
+    "Fax": "2135551234",
+    "Email": "alice.johnson@example.com",
+    "lat": "lat_data",
+    "lng": "lng_data",
+    "LastPracUpdate": "LastPracUpdate_data",
+    "LastPracRoleUpdate": "LastPracRoleUpdate_data",
+    "LastLocationUpdate": "LastLocationUpdate_data",
+    "AccuracyScore": "acc_score_data"
+}]
 
-# Serialize the data to a JSON-formatted string
-serialized_data = json.dumps(data, indent=2)
-print("Serialized Data:")
-print(serialized_data)
-
-# Deserialize the JSON-formatted string back to a Python object
-deserialized_data = json.loads(serialized_data)
-print("\nDeserialized Data:")
-print(deserialized_data)
+# @app.route("/", methods=["GET"])
+# def get_data():
+#     """
+#     Given a list of flattened FHIR data this endpoint will render a table for json object
+#     """
+#
+#     return render_template("app.html", json_data=test_data)
 
 
-"""TODO:"""
-## FirstName
-## for list in objects that contains attribute object.firstname
-##displays all firstname in a table
-
-##Feed in json object through post
-##or be able to accept an object from model and display it
-
-##model view controller
-
-
-@app.route("/")
-def index():
+@app.route("/", methods=["GET", "POST"])  # Controller: what Flask Controls
+def index():  # Model
     """
-    TODO:
-    - Call endpoints from SmartClient?
-    - render the html with the data objects and display it
-
-    :return:
+    Given a list of flattened FHIR data this endpoint will use POST method
+    in order to display the data to the user
+    In order to render that data, we will redirect it to the html file
     """
+    if request.method == "GET":
+        return render_template("app.html", json_data=test_data)
+    elif request.method == "POST":
+        request_json = request.json
+        # View: coding view as app.html with Model data being passed
+        return render_template("app.html", json_data=request_json)
 
-    json_data = json.dumps(data, indent=2)
-    return render_template("public/app.html", json_data=json_data)
+@app.route("/data", methods=["GET", "POST"])
+def get_data():
+    """
+    Endpoint to retrieve data based on key-value pairs passed via GET or POST method
+    """
+    if request.method == "GET":
+        endpoint = request.args.get('Endpoint')
+        data = next((entry for entry in test_data if entry["Endpoint"] == endpoint), None)
+        if data:
+            return jsonify(data)
+            # return render_template("app.html", json_data=jsonify(data))
+        else:
+            return jsonify({"error": f"Data not found for endpoint: {endpoint}"}), 404
 
+    elif request.method == "POST":
+        request_data = request.json
+        response_data = []
 
-"""
-Download list of object to a file by:
-- Calling a function that creates the file
-- Serve the file for download
-"""
+        for item in request_data:
+            endpoint = item.get('Endpoint')
+            data = next((entry for entry in test_data if entry["Endpoint"] == endpoint), None)
 
+            if data:
+                response_data.append(data)
+            else:
+                response_data.append({"error": f"Data not found for endpoint: {endpoint}"})
 
+        return jsonify(response_data)
+        # return render_template("app.html", json_data=jsonify(response_data))
 
-@app.route("/download", methods=["GET"])
+# Method probably needs to be a GET method,
+# we want someone to navigate to it and download the file
+# A POST would get more data secured than a GET method
+@app.route("/download", methods=["GET", "POST"])
 def download_file():
+    """
 
-    # Create a temp JSON file
-    # Give it a specific path
-    temp_file_path = f"data_files/temp_data.json"
-    with open(temp_file_path, "w") as f:
-        json.dump(data, f, indent=2)
+    We want to make a file that lives in memory since some servers in
+    file path doesn't have enough disk space
 
-    # Serve the file for download
-    return send_file(temp_file_path, as_attachment=True)
+    This will accept a json string and converts into list of dictionaries
+    and write to file:
+
+    - Converts dictionary to a string in bytes
+    - Make byte object
+    - Fill utf-8 encoded string as bytes into bytes object
+    - Place bytes into a file
+
+    """
+
+    if request.method == "GET":
+        json_data = test_data
+        json_str = json.dumps(json_data, indent=4)
+        file_bytes = BytesIO()
+        file_bytes.write(json_str.encode('utf-8'))
+        file_bytes.seek(0)
+        return send_file(file_bytes, as_attachment=True, attachment_filename="testfile.json")
+    elif request.method == "POST":
+        # test_data = request.json
+        req_json = request.json
+        # json_data = json.dumps(test_data, indent=4)
+        json_data = json.dumps(req_json, indent=4)
+        file_bytes = BytesIO()
+        file_bytes.write(json_data.encode('utf-8'))
+        file_bytes.seek(0)
+        # Make it redirect
+        return send_file(file_bytes, as_attachment=True, attachment_filename="testfile.json")
 
 
-# Unit test for checking if data matches in the downloaded file
-
-def test_download_same_data():
-    # Simulate a request to the download endpoint
-    response = app.test_client().get('/download')
-
-    # Check if the response status code is 200
-    assert response.status_code == 200
-
-    # Check if the Content-Disposition header is set to attachment
-    assert 'attachment' in response.headers['Content-Disposition']
-
-    # Save the downloaded file content
-    with open("downloaded_data.json", "wb") as f:
-        f.write(response.data)
-
-    # Read the content of the downloaded file
-    with open("downloaded_data.json", "r") as f:
-        downloaded_data = json.load(f)
-
-    # Check if the downloaded data matches the original data
-    assert downloaded_data == data
-
-    # Clean up the temporary file
-    os.remove("downloaded_data.json")
+# TODO: Provide a GET endpoint for each endpoint
+# From POST redirect to a GET and pass in the data
+# using GET req.args
 
 
 if __name__ == "__main__":

6729c616c917f74ed3709e29c5419a505700fc8e
Hla Htun <kshap.karki@gmail.com>
2024-02-02 08:49:27 -0800

    fix: test on FlattenSmartOnFHIRObject now passes

--- src/fhirtypepkg/tests/test_FlattenSmartOnFHIRObject.py
+++ src/fhirtypepkg/tests/test_FlattenSmartOnFHIRObject.py
@@ -6,9 +6,10 @@ def test_constructor_works_as_intended():
     # Arrange
     test_date = datetime.utcnow()
     test_endpoint = "FAKE ENDPOINT"
+
     # Act
-    flatten_smart = FlattenSmartOnFHIRObject(test_endpoint, test_date)
+    flatten_smart = FlattenSmartOnFHIRObject(test_endpoint)
 
     # Assert
     assert flatten_smart.endpoint == test_endpoint
-    assert flatten_smart.date_retrieved == test_date
+    assert flatten_smart.date_retrieved.replace(microsecond=0) == datetime.utcnow().replace(microsecond=0)

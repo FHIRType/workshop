354a8f037de67730ed3886a612294babec657222
ImgyeongLee <mingbab33@gmail.com>
2024-02-08 20:19:05 -0800

    update: Swagger UI /getdata

--- FhirCapstoneProject/swaggerUI/app/__init__.py
+++ FhirCapstoneProject/swaggerUI/app/__init__.py
@@ -6,7 +6,6 @@ from .routers import ns
 def create_app():
     app = Flask(__name__)
     api.init_app(app)
-
     api.add_namespace(ns)
 
     return app
--- FhirCapstoneProject/swaggerUI/app/routers.py
+++ FhirCapstoneProject/swaggerUI/app/routers.py
@@ -3,8 +3,9 @@ from flask import make_response, Flask, render_template, send_file, jsonify
 import json
 from .extensions import api
 from io import BytesIO
-from .models import practitioner
+from .models import practitioner, error
 from FhirCapstoneProject.fhirtypepkg.main import search_practitioner
+from .data import api_description
 
 test_data = (
     {
@@ -30,42 +31,42 @@ test_data = (
         "LastPracUpdate": "LastPracUpdate_data",
         "LastPracRoleUpdate": "LastPracRoleUpdate_data",
         "LastLocationUpdate": "LastLocationUpdate_data",
-        "AccuracyScore": "acc_score_data",
+        "AccuracyScore": 85,
     },
 )
 
-ns = Namespace("api")
+ns = Namespace("api", description='API endpoints related to Practitioner.')
 parser = reqparse.RequestParser()
-parser.add_argument("first_name", type=str, help="The first name of the practitioner")
-parser.add_argument("last_name", type=str, help="The last name of the practitioner")
-parser.add_argument("npi", type=str, help="The NPI of the practitioner")
-parser.add_argument("endpoint", type=str, help="The type of the endpoint")
-parser.add_argument("format", type=str, help="The type of the returned data")
+parser.add_argument("first_name", required=True, type=str, help="The first name of the practitioner")
+parser.add_argument("last_name", required=True, type=str, help="The last name of the practitioner")
+parser.add_argument("npi", required=True, type=str, help="The NPI of the practitioner")
+parser.add_argument("endpoint", action='split', type=str, help="The type of the endpoint (default: All)")
+parser.add_argument("format", type=str, choices=('file', 'page'), help="The type of the returned data - returns JSON format by default.")
 
 
 # api/getdata
-# Description: Given a first name, last name, and NPI
-# the routes retrieve data from all endpoints or specified endpoints.
-# return data as JSON, a file, or web page.
-# Optionally it could contain an attribute to limit or
-# specify the endpoints used to gather data.
-# It should use an object to serialize and de-serialize the flattened data.
 @ns.route("/getdata")
 class GetData(Resource):
-    @ns.doc("Retrieve data from all endpoints or specified endpoints")
     @ns.expect(parser)
-    # @ns.marshal_list_with(practitioner)
+    @ns.response(200, 'The data was successfully retrieved.', practitioner)
+    @ns.response(400, 'Invalid request. Check the required queries.', error)
+    @ns.response(404, 'Could not find the practitioner with given data.', error)
+    @ns.doc(description=api_description["getdata"])
+    @ns.marshal_list_with(practitioner)
     def get(self):
         args = parser.parse_args()
         first_name = args["first_name"]
         last_name = args["last_name"]
         npi = args["npi"]
-        format = args["format"]
+        return_type = args["format"]
+
+        # TODO: Call actual function later
+        # print(search_practitioner(first_name, last_name, npi)[1])
 
         if first_name and last_name and npi:
-            if format == "page":
+            if return_type == "page":
                 return make_response(render_template("app.html", json_data=test_data))
-            elif format == "file":
+            elif return_type == "file":
                 json_data = test_data
                 json_str = json.dumps(json_data, indent=4)
                 file_bytes = BytesIO()

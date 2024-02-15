4ce703c2641862f4ce082c9661fec946961dc252
Trenton Young <trentonyo@gmail.com>
2024-02-15 10:28:23 -0800

    fix: linted smart client tests

--- FhirCapstoneProject/tests/test_smartclient_query_generation.py
+++ FhirCapstoneProject/tests/test_smartclient_query_generation.py
@@ -12,6 +12,7 @@ import fhirclient.models.practitioner as Practitioner
 from FhirCapstoneProject.fhirtypepkg.endpoint import Endpoint
 from FhirCapstoneProject.fhirtypepkg.client import SmartClient
 
+
 @pytest.fixture
 def create_http_enabled_endpoint():
     return Endpoint(
@@ -31,6 +32,7 @@ def create_search_parameters():
         "string": "string",
     }
 
+
 def test_http_build_search(create_search_parameters):
     """
     Tests the basic usage of http_build_search with bools, integers,
@@ -42,17 +44,17 @@ def test_http_build_search(create_search_parameters):
     request = Request(params=output)
     result_values = request.params
 
-    assert result_values[0][0] == 'boolean_true'
-    assert result_values[0][1] == create_search_parameters['boolean_true']
+    assert result_values[0][0] == "boolean_true"
+    assert result_values[0][1] == create_search_parameters["boolean_true"]
 
-    assert result_values[1][0] == 'boolean_false'
-    assert result_values[1][1] == create_search_parameters['boolean_false']
+    assert result_values[1][0] == "boolean_false"
+    assert result_values[1][1] == create_search_parameters["boolean_false"]
 
-    assert result_values[2][0] == 'int'
-    assert result_values[2][1] == create_search_parameters['int']
+    assert result_values[2][0] == "int"
+    assert result_values[2][1] == create_search_parameters["int"]
 
-    assert result_values[3][0] == 'string'
-    assert result_values[3][1] == create_search_parameters['string']
+    assert result_values[3][0] == "string"
+    assert result_values[3][1] == create_search_parameters["string"]
 
 
 def test_fhir_build_search(create_search_parameters):
@@ -62,9 +64,9 @@ def test_fhir_build_search(create_search_parameters):
     by checking that the params generated from a Request match those supplied to the http_build_search function.
     """
     arbitrary_resource = Practitioner.Practitioner
-    output = ClientNamespace.fhir_build_search(arbitrary_resource, create_search_parameters)
+    output = ClientNamespace.fhir_build_search(
+        arbitrary_resource, create_search_parameters
+    )
 
     for param in output.params:
         assert create_search_parameters[param.name] == param.value
-
-

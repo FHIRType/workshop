40b0a7be114c291b52956dace7b68ac157ea188f
Hla Htun <kshap.karki@gmail.com>
2024-03-07 10:42:08 -0800

    fix: routers.py clean up and validation of flatten_data

--- FhirCapstoneProject/swaggerUI/app/routers.py
+++ FhirCapstoneProject/swaggerUI/app/routers.py
@@ -16,33 +16,6 @@ from io import BytesIO
 from .models import practitioner
 from .utils import validate_inputs
 
-test_data = {
-    "Endpoint": "testEndpoint",
-    "DateRetrieved": "01-21-2024",
-    "FullName": "John Smith",
-    "NPI": "0112031311",
-    "FirstName": "John",
-    "LastName": "Smith",
-    "Gender": "Male",
-    "Taxonomy": "X02332D2",
-    "GroupName": "Orthodontist",
-    "ADD1": "1234 SW Ave",
-    "ADD2": "5678 NW Ave",
-    "City": "Chicago",
-    "State": "Illinois",
-    "Zip": "12234",
-    "Phone": "9712131234",
-    "Fax": "5031231234",
-    "Email": "abc@gmail.com",
-    "lat": "lat_data",
-    "lng": "lng_data",
-    "LastPracUpdate": "LastPracUpdate_data",
-    "LastPracRoleUpdate": "LastPracRoleUpdate_data",
-    "LastLocationUpdate": "LastLocationUpdate_data",
-    "AccuracyScore": "85",
-}
-
-
 ns = Namespace("api", description="API endpoints related to Practitioner.")
 
 
@@ -61,36 +34,27 @@ class GetData(Resource):
         npi = args["npi"]
         return_type = args["format"]
 
-        practitioner_all_results, flatten_practitioner_data = search_practitioner(
-            last_name, first_name, npi
-        )
-        # role_all_results, fatten_role_data = search_practitioner_role(last_name, first_name, npi)
-        # TODO: Call actual function later
         flatten_data = search_all_practitioner_data(last_name, first_name, npi)
 
         # Validate the user's queries
         # If they are invalid, throw status code 400 with an error message
-        validation_result = validate_inputs(test_data)
-        if not validation_result["success"]:
-            # abort(validation_result["status_code"], message=validate_inputs(test_data)["message"])
-            abort(
-                validation_result["status_code"],
-                message=validate_inputs(flatten_data)["message"],
-            )
-
         if first_name and last_name and npi:
-            if flatten_data is None:
+            if flatten_data is None or len(flatten_data) < 1:
                 abort(404, "Didn't find anyone!")
             else:
+                for data in flatten_data:
+                    validation_result = validate_inputs(data)
+                    if not validation_result["success"]:
+                        abort(
+                            validation_result["status_code"],
+                            message=validate_inputs(data)["message"],
+                        )
                 if return_type == "page":
-                    # return make_response(render_template("app.html", json_data=test_data))
-                    # TODO: This is a holdover to suppress role
                     return make_response(
                         render_template("app.html", json_data=flatten_data)
                     )
                 elif return_type == "file":
                     json_data = flatten_data
-                    # json_data = test_data
                     json_str = json.dumps(json_data, indent=4)
                     file_bytes = BytesIO()
                     file_bytes.write(json_str.encode("utf-8"))

1b5f6fb837831b713956c99f0245d98547a3db5e
ImgyeongLee <mingbab33@gmail.com>
2024-03-08 11:16:11 -0800

    fix: cors policy

--- FhirCapstoneProject/swaggerUI/app/__init__.py
+++ FhirCapstoneProject/swaggerUI/app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from .extensions import api
 from .routers import ns
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+CORS(app)
 api.init_app(app)
 api.add_namespace(ns)

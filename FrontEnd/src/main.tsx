8a1fe83be5db1d1852283cd84116f82bc0d3ca1b
valdovjo <valdovjo@oregonstate.edu>
2024-03-15 11:13:55 -0700

    fix: now queries practitioner by first name, last name, and npi

--- FrontEnd/src/main.tsx
+++ FrontEnd/src/main.tsx
@@ -19,7 +19,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <QueryClientProvider client={queryClient}>
           <RouterProvider router={router}/>
       </QueryClientProvider>
-      {/*<App />*/}
 
   </React.StrictMode>,
 )

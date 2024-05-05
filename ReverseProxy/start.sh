0c1fbe1bc1cc38081947264c944784f860d6d39c
TrentonYo <trentonyo@gmail.com>
2024-05-05 11:50:28 -0700

    deploy: frontend is served through proxy

--- ReverseProxy/start.sh
+++ ReverseProxy/start.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-envsubst "$FLASK_SERVER_ADDR" < /tmp/default.conf > /etc/nginx/conf.d/default.conf && nginx -g 'daemon off;'
+envsubst '$API_BASE_URL$FRONTEND_BASE_URL' < /etc/nginx/conf.d/default.conf > /tmp/_default.conf && \
+mv -f /tmp/_default.conf /etc/nginx/conf.d/default.conf && \
+nginx -g 'daemon off;'

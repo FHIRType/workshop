0c182d45eb462225bbe1f4e36a9015c4381b73f9
TrentonYo <trentonyo@gmail.com>
2024-05-05 12:56:16 -0700

    deploy: docs are now served as well (but they aren't compiled for some reason)

--- ReverseProxy/start.sh
+++ ReverseProxy/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-envsubst '$API_BASE_URL$FRONTEND_BASE_URL$BASE_HOST' < /etc/nginx/conf.d/default.conf > /tmp/_default.conf && \
+envsubst '$API_BASE_URL$FRONTEND_BASE_URL$BASE_HOST$DOCS_STATIC' < /etc/nginx/conf.d/default.conf > /tmp/_default.conf && \
 mv -f /tmp/_default.conf /etc/nginx/conf.d/default.conf && \
 nginx -g 'daemon off;'

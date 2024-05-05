0a7f2222f745483ab2ec007c1d00f46add967e6a
TrentonYo <trentonyo@gmail.com>
2024-05-05 11:09:49 -0700

    deploy: api and frontend are hooked up

--- FhirCapstoneProject/wsgi.py
+++ FhirCapstoneProject/wsgi.py
@@ -1,5 +1,8 @@
-from FhirCapstoneProject.swaggerUI.app import app
 import os
+import sys
+
+from FhirCapstoneProject.swaggerUI.app import app
 
 if __name__ == "__main__":
+    # print(app.config, file=sys.stderr)
     app.run(host="0.0.0.0", port=os.environ.get("FLASK_SERVER_PORT"), debug=True)

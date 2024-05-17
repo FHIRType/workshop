e79e8757bea142336785d08d7618b2a93041733c
Trenton Young <trentonyo@gmail.com>
2024-05-17 13:16:09 -0700

    trying to get vite deployed with actions

--- FrontEnd/pages-vite.config.ts
+++ FrontEnd/pages-vite.config.ts
@@ -2,7 +2,22 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default ({
   plugins: [react()],
-  base: "/landing-page/"
+  preview: {
+    port: 8080,
+    strictPort: true
+  },
+  server: {
+    port: 8080,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:8080"
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    tsconfigRootDir: __dirname,
+  },
 })
